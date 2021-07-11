f4f68afdd46ce137e4b1313f6f53fc9ebed02151
Kaname <kaname.g@gmail.com>
2021-07-11 14:33:08 +0900

    リモート接続まで出来るようにバグ修正

--- python/src/sample.py
+++ python/src/sample.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import numpy a snp
+import numpy as np
 
 #x軸:時刻
 x = np.arange(0, 100, 0.5)
@@ -11,5 +11,3 @@ y = np.sin(2.0 * np.pi * (x * Hz) / 100)
 #グラフを描画
 plt.plot(x, y)
 plt.savefig('test.png')
-
-
